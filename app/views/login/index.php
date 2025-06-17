b6809c1632dba29c7e2714696056ff4dc9cb8c87
Harmanjot14 <jotkharman@gmail.com>
2025-06-17 21:40:39 +0000

    Back to old one

--- app/views/login/index.php
+++ app/views/login/index.php
@@ -2,19 +2,29 @@
 
 <?php session_start(); ?>
 
-<main role="main" class="login-container">
-		<h1>Login Forum</h1>
-		<form action="/validate.php" method="post">
-			<label for="username">Username:</label>
-			<br>
-			<input type="text" id="username" name="username">
-			<br>
-			<label for="password">Password:</label>
-			<br>
-			<input type="password" id="password" name="password">
-			<br><br>
-			<button type="submit">Login</button>	
-		</form>
+<main role="main" class="container">
+  <div class="page-header" id="banner">
+      <div class="row">
+        <div>
+           <h1>You are not logged in</h1>
+        </div>
+       </div>
+  </div>
+
+	<div class="row">
+    <div>
+			<form action="/login/verify" method="post" >
+				<div class="form-group">
+					<label for="username">Username</label>
+					<input required type="text" class="form-control" name="username">
+				</div>
+				<div class="form-group">
+					<label for="password">Password</label>
+					<input required type="password" class="form-control" name="password">
+				</div>
+	            <br>
+			    <button type="submit">Login</button>	
+			</form> 
 			<p>Don't have an account? <a href ="/create/index">Sign up</a></p>
 		</div>
 	</div>
