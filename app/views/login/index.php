e496d5e4a16e5f24375a527e6e9361b6cb78f5cd
Harmanjot14 <jotkharman@gmail.com>
2025-06-17 21:33:14 +0000

    Added my old login page from Assignment 2

--- app/views/login/index.php
+++ app/views/login/index.php
@@ -2,35 +2,24 @@
 
 <?php session_start(); ?>
 
-<main role="main" class="container">
-    <div class="page-header" id="banner">
-        <div class="row">
-            <div class="col-lg-12">
-                <h1>You are not logged in</h1>
-            </div>
-        </div>
-    </div>
-
-<div class="row">
-    <div class="col-sm-auto">
-		<form action="/login/verify" method="post" >
-		<fieldset>
-			<div class="form-group">
-				<label for="username">Username</label>
-				<input required type="text" class="form-control" name="username">
-			</div>
-			<div class="form-group">
-				<label for="password">Password</label>
-				<input required type="password" class="form-control" name="password">
-			</div>
-            <br>
-		    <button type="submit" class="btn btn-primary">Login</button>
-		</fieldset>
-		</form> 
-		<p>Don't have an account? <a href ="/create/index">Sign up</a></p>
+<main role="main" class="login-container">
+		<h1>Login Forum</h1>
+		<form action="/validate.php" method="post">
+			<label for="username">Username:</label>
+			<br>
+			<input type="text" id="username" name="username">
+			<br>
+			<label for="password">Password:</label>
+			<br>
+			<input type="password" id="password" name="password">
+			<br><br>
+			<button type="submit">Login</button>	
+		</form>
+			<p>Don't have an account? <a href ="/create/index">Sign up</a></p>
+		</div>
 	</div>
-</div>
-    <?php require_once 'app/views/templates/footer.php' ?>
+	
+  <?php require_once 'app/views/templates/footer.php' ?>
 	
 	<?php if (isset($_SESSION['error'])): ?>
 		<p style="color: red;"><?php echo $_SESSION['error']; ?></p>
